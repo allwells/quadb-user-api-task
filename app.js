915632e99d15dbdb32aada78be8f54de9bb38a8a
allwells <aleenfestus@gmail.com>
2022-01-21 16:33:24 +0100

    WIP: Create middlewares
    Signed-off-by: allwells <aleenfestus@gmail.com>

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 require("express-async-errors")
+const csrf = require("csurf")
 const express = require("express")
+const helmet = require("helmet")
 const app = express()
 
 
+app.use(helmet())
+app.use(csrf({ cookie: true }))
 app.use(express.json())
 
 module.exports = app
